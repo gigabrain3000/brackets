e6cfec2f21b2b5d3adbf7dc42cc7d352512d0242
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2024-09-28 19:27:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = true;
+  let stack = [];
+  for (let i = 0 ; i < str.length; i++) {
+    bracketsConfig.forEach(element => {
+      if (element[0] === str[i] && !stack.includes(element[1])) {
+        stack.push(str[i]);
+      } else if (element[1] === str[i] && stack.length > 0 && element.includes(stack[stack.length - 1])) {
+        stack.pop();
+      } else if (element[1] === str[i] && stack.length === 0) {
+        console.log(str[i]);
+        return result = false;
+      }
+    });
+  }
+  return stack.length === 0 && result ? true : false;
 }
